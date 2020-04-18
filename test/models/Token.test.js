520e85e86e31df3ffe1a577f187837dd1d7462c8
chrispiccaro <chrispiccaro18@gmail.com>
2020-04-18 14:55:45 -0700

    basic auth flow

---
+++ test/models/Token.test.js
@@ -0,0 +1,31 @@
+ 
+require('../connect-db');
+const Token = require('../../lib/models/Token');
+
+describe('Token model tests', () => {
+  it('has a _id, code, access token, expires at, expires in, refresh token, scope, and token type', () => {
+    const now = new Date();
+    const expires_at = now.setSeconds(now.getSeconds() + 3599);
+    const token = new Token({
+      _id: '1234',
+      code: '1234',
+      access_token: 'long ass string',
+      expires_at,
+      expires_in: 3599,
+      refresh_token: 'long ass string',
+      scope: 'account:master account:read:admin account:write:admin contact:read:admin meeting:master meeting:read:admin meeting:write:admin user:master user:read:admin user:write:admin webinar:master webinar:read:admin webinar:write:admin',
+      token_type: 'bearer',
+    });
+
+    expect(token.toJSON()).toEqual({
+      _id: '1234',
+      code: '1234',
+      access_token: 'long ass string',
+      expires_at,
+      expires_in: 3599,
+      refresh_token: 'long ass string',
+      scope: 'account:master account:read:admin account:write:admin contact:read:admin meeting:master meeting:read:admin meeting:write:admin user:master user:read:admin user:write:admin webinar:master webinar:read:admin webinar:write:admin',
+      token_type: 'bearer',
+    });
+  });
+});
